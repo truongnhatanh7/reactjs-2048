226a546575917b78d9668e12a5f6c90141be510a
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-06-06 14:24:12 +0700

    Update Game.js

--- 2048/src/components/Game.js
+++ 2048/src/components/Game.js
@@ -17,6 +17,7 @@ function moveLeft(board) {
             }
         }
     }
+    
     return JSON.parse(JSON.stringify(board))
 
 }
@@ -27,7 +28,101 @@ function mergeLeft(board) {
             if (board[row][col - 1] === board[row][col]) {
                 board[row][col - 1] *= 2
                 board[row][col] = 0
-                console.log("Merge:", board)
+            }
+        }
+    }
+    return JSON.parse(JSON.stringify(board))
+}
+
+function moveRight(board) {
+    for (let row = 0; row < board.length; row++) {
+        for (let col = board.length - 2; col >= 0; col--) {
+            let tempCol = col
+            while (true) {
+                if (tempCol < board.length && board[row][tempCol + 1] === 0) {
+                    let temp = board[row][tempCol]
+                    board[row][tempCol] = 0
+                    board[row][tempCol + 1] = temp
+                    tempCol += 1 
+                } else {
+                    break
+                }
+            }
+        }
+    }
+    return JSON.parse(JSON.stringify(board))
+}
+
+function mergeRight(board) {
+    for (let row = 0; row < board.length; row++) {
+        for (let col = board.length - 2; col >= 0; col--) {
+            if (board[row][col + 1] === board[row][col]) {
+                board[row][col + 1] *= 2
+                board[row][col] = 0
+            }
+        }
+    }
+    return JSON.parse(JSON.stringify(board))
+}
+
+function moveUp(board) {
+    for (let col = 0; col < board.length; col++) {
+        for (let row = 1; row < board.length; row++) {
+            let tempRow = row
+            while (true) {
+                if (tempRow > 0 && board[tempRow - 1][col] === 0) {
+                    let temp = board[tempRow][col]
+                    board[tempRow][col] = 0
+                    board[tempRow - 1][col] = temp
+                    tempRow -= 1
+                } else {
+                    break
+                }
+            }
+        }
+    }
+    return JSON.parse(JSON.stringify(board))
+}
+
+function mergeUp(board) {
+    for (let col = 0; col < board.length; col++) {
+        for (let row = 1; row < board.length; row++) {
+            if (board[row - 1][col] === board[row][col]) {
+                board[row - 1][col] *= 2
+                board[row][col] = 0
+            }
+        }
+    }
+    return JSON.parse(JSON.stringify(board))
+}
+
+
+function moveDown(board) {
+    for (let col = 0; col < board.length; col++) {
+        for (let row = board.length - 2; row >= 0; row--) {
+            let tempRow = row
+            while (true) {
+                console.log(tempRow, col, board)
+                if (tempRow < board.length - 1 && board[tempRow + 1][col] === 0) {
+                    let temp = board[tempRow][col]
+                    board[tempRow][col] = 0
+                    board[tempRow + 1][col] = temp
+                    tempRow += 1
+                } else {
+                    break
+                }
+            }
+        }
+    }
+    return JSON.parse(JSON.stringify(board))
+}
+
+function mergeDown(board) {
+    for (let col = 0; col < board.length; col++) {
+        for (let row = board.length - 2; row >= 0; row--) {
+            if (board[row + 1][col] === board[row][col]) {
+                board[row + 1][col] *= 2
+                board[row][col] = 0
             }
         }
     }
@@ -46,14 +141,26 @@ export default function Game() {
 
     useEffect(() => {
         window.addEventListener('keydown', (event) => {
-            console.log(event.key)
             if (event.key === "ArrowLeft") {
                 board = moveLeft(board)
                 board = mergeLeft(board)
                 board = moveLeft(board)
-                console.log("Finish merge", board)
-                setBoard(board)
+            } else if (event.key === "ArrowRight") {
+                board = moveRight(board)
+                board = mergeRight(board)
+                board = moveRight(board)
+
+            } else if (event.key === "ArrowUp") {
+                board = moveUp(board)
+                board = mergeUp(board)
+                board = moveUp(board)
+            } else if (event.key === "ArrowDown") {
+                console.log(board)
+                board = moveDown(board)
+                board = mergeDown(board)
+                board = moveDown(board)
             }
+            setBoard(board)
         })
     }, [])
 
